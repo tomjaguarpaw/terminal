8b0b751f882165fe08cbd12cd3fc96674f874dc1
Tom Ellis <tom-2013@purelyagile.com>
2015-04-18 21:40:22 +0100

    Flow sigs

--- Proxy.hs
+++ Proxy.hs
@@ -16,23 +16,34 @@ data Socket a b r = Same a (b -> r)
 echo :: AwaitUpstream (Socket a a) f r
 echo = AwaitUpstream (\(Same a f) -> RespondUpstream (f a) echo)
 
-(>->) :: Functor h => AwaitUpstream f g r -> AwaitUpstream g h (Active f g r) -> AwaitUpstream f h r
+(>->) :: Functor h =>
+         AwaitUpstream f g r
+      -> AwaitUpstream g h (Active f g r)
+      -> AwaitUpstream f h r
 (>->) (AwaitUpstream f) (AwaitUpstream g) = AwaitUpstream $ \h ->
   case f h of
     RespondUpstream   r  next -> RespondUpstream r (next >-> AwaitUpstream g)
     RequestDownstream dfact   -> activateDowner dfact (AwaitUpstream g)
   
-activateUpper :: Functor h => Active f g r -> AwaitUpstream g h (Active f g r) -> Active f h r
+activateUpper :: Functor h =>
+                 Active f g r
+              -> AwaitUpstream g h (Active f g r)
+              -> Active f h r
 activateUpper f g = case f of
   RespondUpstream r await -> RespondUpstream r (await >-> g)
   RequestDownstream fact  -> activateDowner fact g
 
-activateDowner :: Functor h => g (Active f g r) -> AwaitUpstream g h (Active f g r) -> Active f h r
+activateDowner :: Functor h =>
+                  g (Active f g r)
+               -> AwaitUpstream g h (Active f g r)
+               -> Active f h r
 activateDowner f (AwaitUpstream g) = case g f of
   RespondUpstream r await -> activateUpper r await
   RequestDownstream hact -> sendDownstream hact
 
-sendDownstream :: Functor h => h (Active g h (Active f g r)) -> Active f h r
+sendDownstream :: Functor h =>
+                  h (Active g h (Active f g r))
+               -> Active f h r
 sendDownstream h = RequestDownstream $ flip fmap h $ \case
   RespondUpstream r await  -> activateUpper r await
   RequestDownstream dfact -> sendDownstream dfact
